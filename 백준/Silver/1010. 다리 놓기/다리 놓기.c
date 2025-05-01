85abb84892962a5debb51acfffe42b7aacb678ae
MYUNG HYUN YOU <129754242+YoMyungHyun@users.noreply.github.com>
2025-05-01 23:10:29 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+	int k,n,m,dam;
+	scanf("%d", &k);
+	for(int z=0; z<k; z++)
+	{
+		dam=1;
+		scanf("%d %d", &n, &m);
+		for(int i=0; i<n; i++)
+		{
+			dam*=m-i; 
+			dam/=1+i;
+		}
+		printf("%d\n", dam);
+	}
+}
