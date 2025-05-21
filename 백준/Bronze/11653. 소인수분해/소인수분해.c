ae2505b0a1e6e436baf54108fc07df588c8d5b76
MYUNG HYUN YOU <129754242+YoMyungHyun@users.noreply.github.com>
2025-05-21 19:55:52 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+ 
+int main(void){
+    int N;
+    scanf("%d", &N);
+ 
+    while(N>1){
+        for(int i=2; i<=N; i++){
+            if(N%i==0){
+                printf("%d\n", i);
+                N = N/i;
+                break; //다시 2부터 시작하기 위해
+            }
+        }
+    }
+}
