c7d8764908faaff265fe0c267dbefd694b3f4a20
MYUNG HYUN YOU <129754242+YoMyungHyun@users.noreply.github.com>
2025-05-28 16:42:16 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+ 
+int main() {
+	int N, K, com = 1, i;
+	scanf("%d %d", &N, &K);
+	for (i = 0; i < K; i++) {
+		com *= N;
+		N--;
+	}
+	for (i = K; i > 0; i--)
+		com /= i;
+	printf("%d", com);
+	return 0;
+}
